f936b99c8034c23525a6b2652166d89ea23f6c24
ShivamBavda0411 <shivambavda003@gmail.com>
2026-04-02 21:30:44 +0530

    add hero

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Hero.css'
+import product1 from '../../assets/Product01.jpeg'
+import product2 from '../../assets/Product02.jpeg'
+
+const Hero = () => {
+  return (
+     <section className="hero">
+      <div className="hero-container">
+
+        {/* Left Content */}
+        <div className="hero-text">
+          <h1>Premium Pooja Powder Manufacturer</h1>
+          <p>
+            We provide high-quality pooja powders made with purity and devotion.
+            Our products are perfect for all religious rituals and ceremonies.
+          </p>
+
+          <button className="hero-btn">View Products</button>
+        </div>
+
+        {/* Right Images */}
+        <div className="hero-images">
+          <img src={product1} alt="Pooja Powder 1" />
+          <img src={product2} alt="Pooja Powder 2" />
+        </div>
+
+      </div>
+    </section>
+  )
+}
+
+export default Hero
